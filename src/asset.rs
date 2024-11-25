5f22a00ea6c3d3451eedc8dbeead5aef8fbc9eb0
Min <minhnguyen1995@gmail.com>
2024-11-25 15:26:15 +0000

    upd

--- src/asset.rs
+++ src/asset.rs
@@ -1,23 +1,20 @@
-// pub enum AssetType {
-//     Equity,
-//     Debt,
-//     Derivative,
-//     Commodity,
-//     Currency,
-// }
+/* Financial assets which are quantiable */
 
+use chrono::NaiveDateTime;
+
+// Share amount of a company
 pub struct StockAsset {
-    symbol: String, //  ticker symbol for the stock (e.g., "AAPL" for Apple, "GOOG" for Google).
-    company: String,
+    symbol: String, // e.g., "AAPL" for Apple, "GOOG" for Google
 }
-
+// Loan amount to a bond issuer (e.g. company), expecting a returned interest rate
 pub struct BondAsset {
-    issuer: String,
+    issuer: String, // Issuer of the bond, e.g., "Tesla" or "US Treasury"
+    currency: String,
+    face_value: f64,              // Nominal value of the bond
+    interest_rate: f64,           // The annual interest rate (coupon rate), e.g., 5%
+    maturity_date: NaiveDateTime, // The date the bond matures (e.g., "2025-12-31")
 }
-
-pub enum CashAsset {
-    USD,
-    EUR,
-    BTC,
-    ETH,
+// Cash amount under a currency
+pub struct CashAsset {
+    currency: String, // e.g. USD,  EUR,  BTC,  ETH
 }
